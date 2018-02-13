5db01064cca745312199f6f27d85caacc278a63a
Karen Chen <31599065+kkc2017@users.noreply.github.com>
2018-02-13 02:43:29 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 # Get the temperature and weather information for your current location (worldwide) from Weather Underground's API
 # insert the latitude and longitude of your current location into Weather Underground's API
 # Weather Underground's API documentation: https://www.wunderground.com/weather/api/d/docs?d=data/conditions&MR=1
-# Example: http://api.wunderground.com/api/ea46d042f9697f4e/conditions/q/42.36800751795526,-71.11559132022597.json
+# Example: http://api.wunderground.com/api/9af690c1b08dcb17/conditions/q/42.36800751795526,-71.11559132022597.json
 
 import urllib.request, json
 
